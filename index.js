d98e1cfbb67bddeefccf98043af000dc9e37b074
daniaalrababa9 <rababadania@gmail.com>
2020-01-22 19:18:46 +0200

    accept the file name as command line parameter

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 const fs=require('fs');
 const readerAndWriter =require('./edit-file');
 const file = `${__dirname}/files/data/file.txt`;
-// fs.readFile(file,(err,data)=>{
-//     if(err){throw err;}
-//     console.log('callBack',data.toString())
-// })
-
-// fs.writeFile(file, 'dania', function (err) {
-//     if (err) throw err;
-//     console.log('Replaced!');
-//   });
-readerAndWriter.readerWithCallback(file, (err, data) => {
-    if (err) { throw err; }
-    console.log('Callback from module', data);
+fs.readFile(process.argv[2],(err,data)=>{
+    if(err){throw err;}
+    console.log('callBack',data.toString())
+})
+fs.writeFile(process.argv[2], 'dania', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
   });
-   readerAndWriter.writerWithCallback (file,'hello',(err)=>{
+readerAndWriter.readerWithCallback(process.argv[2], (err, data) => {
+    if (err) { throw err; }
+    console.log('Callback from module', data.toString());
+  });
+   readerAndWriter.writerWithCallback (process.argv[2],'hi',(err)=>{
     if (err) throw err;
     console.log('Replaced!')
   
 });
+
+
+// /mnt/c/Users/std21/Lab-Class-03/files/data/file.txt
