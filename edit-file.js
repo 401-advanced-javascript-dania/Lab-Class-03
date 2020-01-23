abfa020c2557fa2455ed412389c5bdc71d76e7ec
daniaalrababa9 <rababadania@gmail.com>
2020-01-23 20:33:09 +0200

    write the read and the write fs with promises

--- edit-file.js
+++ edit-file.js
@@ -1,19 +1,38 @@
 'use strict';
 const fs=require('fs');
-const readerWithCallback =(file,callback)=>{
-    fs.readFile(file,(err,data)=>{
-        if(err){callback(err)}
-        else{callback(undefined,data.toString().trim());}
-    });
-};
-const writerWithCallback =(file,word,callback)=>{
-    fs.writeFile(file,word,(err,data)=>{
-        if(err){callback(err)}
+const util =require('util');
+// const readerWithCallback =(file,callback)=>{
+//     fs.readFile(file,(err,data)=>{
+//         if(err){callback(err)}
+//         else{callback(undefined,data.toString().trim());}
+//     });
+// };
+// const writerWithCallback =(file,word,callback)=>{
+//     fs.writeFile(file,word,(err,data)=>{
+//         if(err){callback(err)}
 
-        else{
-            console.log(data)
-            callback(data)}
-    });
-};
-
-module.exports={readerWithCallback,writerWithCallback};
+//         else{
+//             console.log(data)
+//             callback(data)}
+//     });
+// };
+const readFile =util.promisify(fs.readFile);
+const readerWithPromise=(file)=>{
+    return readFile(file)
+     .then((data)=>{
+         return data.toString().trim()
+     })
+     .catch(err=>console.error(err))
+}
+const writeFile =util.promisify(fs.writeFile);
+const writerWithPromise=(file,data)=>{
+     writeFile(file,data)
+     .then((person)=>{
+         console.log('dd',person)
+         let dataBeforModifing=person.toString().trim()
+         return dataBeforModifing.name=data
+     })
+     .catch(err=>console.error(err))
+}
+// module.exports={readerWithCallback,writerWithCallback}
+module.exports={readerWithPromise,writerWithPromise};
--- index.js
+++ index.js
@@ -5,23 +5,32 @@ const readerAndWriter =require('./edit-file');
 const file='/mnt/c/Users/std21/Lab-Class-03/files/data/person.json';
 //fs function 
 
-fs.readFile(process.argv[2],(err,data)=>{
-    if(err){throw err;}
-    console.log('callBack',data.toString())
-})
-fs.writeFile(process.argv[2], 'dania', function (err) {
-    if (err) throw err;
-    console.log('Replaced!');
-  });
-// callback function 
-readerAndWriter.readerWithCallback(process.argv[2], (err, data) => {
-    if (err) { throw err; }
-    let obj=JSON.parse(data);
-    console.log('Callback from module', obj.firstName);
-  })
+// fs.readFile(process.argv[2],(err,data)=>{
+//     if(err){throw err;}
+//     console.log('callBack',data.toString())
+// })
+// fs.writeFile(process.argv[2], 'dania', function (err) {
+//     if (err) throw err;
+//     console.log('Replaced!');
+//   });
+// // callback function 
+// readerAndWriter.readerWithCallback(process.argv[2], (err, data) => {
+//     if (err) { throw err; }
+//     let obj=JSON.parse(data);
+//     console.log('Callback from module', obj.firstName);
+//   })
 
-readerAndWriter.writerWithCallback (process.argv[2],'hi',(err)=>{
-if (err) throw err; console.log('Replaced!')})
+// readerAndWriter.writerWithCallback (process.argv[2],'hi',(err)=>{
+// if (err) throw err; console.log('Replaced!')})
 
+readerAndWriter.readerWithPromise(process.argv[2])
+.then(data=>console.log('data',JSON.parse(data)))
+.catch(err=>{throw err;})
+
+
+readerAndWriter.writerWithPromise(process.argv[2],'dania')
+.then((data)=>{
+console.log('done',data )})
+.catch(err=>{throw err;})
 
 // /mnt/c/Users/std21/Lab-Class-03/files/data/file.txt
